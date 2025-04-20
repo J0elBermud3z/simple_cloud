9e9a32cfd1951250bc800c79ffcb7224ad670637
Joel <joelnicolasbermudezdiaz@gmail.com>
2025-04-20 03:48:30 -0300

    feat: agregando blueprint del controller home

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,17 @@ from flask import Flask
 from .config import Paths
 from .config import Config
 from .controllers.files_controller import file_bp
+from .controllers.home_controller import home_bp
+
 import os
 
 def create_app():
     
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='views')
     app.config.from_object(Paths)
     app.config.from_object(Config)
 
     app.register_blueprint(file_bp)
+    app.register_blueprint(home_bp)
 
     return app
