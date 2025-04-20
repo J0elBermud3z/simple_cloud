ae398594fad8f3d024308c6a9e957f322456aa92
Joel <joelnicolasbermudezdiaz@gmail.com>
2025-04-20 02:33:34 -0300

    feat: agregando funcionalidad para subir archivos
    En files_controller se hizo un endpoint llamado 'upload'. Para usarse se le debe hacer una peticion POST y adjuntar un archivo, el mismo se subira a uploaded_files/

--- app/controllers/files_controller.py
+++ app/controllers/files_controller.py
@@ -1,12 +1,34 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+import os
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 from flask import current_app
 
-
 file_bp = Blueprint('file', __name__,url_prefix='/file') 
 
-@file_bp.route('/upload')
+def allowed_file(filename):
+
+    ext = os.path.splitext(filename)[1].lower()
+    return ext in current_app.config['ALLOWED_EXTENSIONS']
+
+
+@file_bp.route('/upload', methods=['GET','POST'])
 def upload_file():
 
-    UPLOADED_FILES_PATH = current_app.config['UPLOADED_FILES']
-    return f'Testing {UPLOADED_FILES_PATH}'
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            # flash('Not file part') 
+            return redirect('/')
+        
+        file = request.files['file']
+
+        if file.filename == '':
+            # flash('No selected file')
+            return redirect('/')
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(current_app.config['UPLOADED_FILES'],filename))
+            return 'File uploaded successfully'
+    
+    return redirect('/')
+
