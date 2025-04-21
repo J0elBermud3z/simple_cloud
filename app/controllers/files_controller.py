59695db8aeab237028ddcd422025ee1591012d53
Joel <joelnicolasbermudezdiaz@gmail.com>
2025-04-20 22:09:32 -0300

    refactor: renombrado endpoint de archivos a 'api'

--- app/controllers/files_controller.py
+++ app/controllers/files_controller.py
@@ -3,7 +3,21 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 from flask import current_app
 
-file_bp = Blueprint('file', __name__,url_prefix='/file') 
+file_bp = Blueprint('api', __name__) 
+
+def get_filetype(file:str):
+    file_extension = file.split('.')[1]
+    
+    if len(file) >= 7:
+        file = file[0:4] + '...'
+
+    if file_extension in current_app.config['IMAGE_EXTENSIONS']:
+        return ('image.svg',file)
+    
+    if file_extension in current_app.config['TEXT_EXTENSIONS']:
+        return ('file-text.svg',file)
+    
+    
 
 @file_bp.route('/upload', methods=['GET','POST'])
 def upload_file():
@@ -39,4 +53,24 @@ def delete_file(file_name):
         return '¡File deleted successfully!'
     
     return redirect('/')
+ 
+@file_bp.route('/', methods=['GET'])
+def all_files():
+
+    all_files = {}
+    files = os.listdir(current_app.config['UPLOADED_FILES'])
+    files.sort()
+
+    page = request.args.get('page', 1, type=int) # Obtengo la pagina (?page=1)
+    per_page = 20 # Limito la cantidad de resultados
+    total = len(files) 
+    start = (page - 1) * per_page # Obtengo 
+    end = start + per_page
+
+    files_paginated = files[start:end]
+
+    for file in files_paginated:
+        all_files[file] = get_filetype(file)
+    
+    return render_template('index.html',all_files=all_files, page=page, total=total, per_page=per_page)
 
