71cdef474894b447cb7d5175d7822fb4992631b0
Joel <joelnicolasbermudezdiaz@gmail.com>
2025-04-20 22:43:39 -0300

    feat: mejorar gestión de errores
    Por seguridad, todos los códigos de error se redirigen a 200 para evitar exploraciones al servidor.

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,22 @@
-from flask import Flask
+from flask import Flask, redirect
 from .config import Paths
 from .config import Config
 from .controllers.files_controller import file_bp
 from .controllers.home_controller import home_bp
+from .controllers.error_handlers import register_error_handlers
 
 import os
 
 def create_app():
     
     app = Flask(__name__, template_folder='views')
+    
     app.config.from_object(Paths)
     app.config.from_object(Config)
 
     app.register_blueprint(file_bp)
     app.register_blueprint(home_bp)
+
+    register_error_handlers(app)
 
     return app
