f17a810400297b288d3d42c7fbe0280305ef2bee
Joel <joelnicolasbermudezdiaz@gmail.com>
2025-05-10 19:26:29 -0300

    refactor: arreglando logica para reflejar los archivos

--- app/static/js/filesManager.js
+++ app/static/js/filesManager.js
@@ -1,3 +1,6 @@
+
+let path = '';
+
 function getFileIcon(fileExtension) {
 
     let IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'tif', 'webp', 'svg', 'ico', 'heic', 'avif'];
@@ -8,48 +11,80 @@ function getFileIcon(fileExtension) {
     };
 }
 
-function  refreshFiles() {
+
+function generateDirectoriesHTML(response,pathDirectory) {
+
+    let htmlDirectories = '';
+    let directories = response['directories'];
+    for (let indexDirectory = 0; indexDirectory < directories.length; indexDirectory++) {
+
+        const folder = directories[indexDirectory];
+        const folderName = Object.keys(folder)[0];
+        const haveDirectories = Object.values(folder)[0];
+
+        htmlDirectories +=             `<div class="col text-center">
+        <img onclick=refreshFiles("/api${pathDirectory}/${folderName}") src="/static/img/${ haveDirectories === true ? 'folder_with_files.png' : 'open-folder.png' }" alt="Logo" width="55" height="55"
+            style="margin-top: 10px;">
+        <h5 class="text-dracula fs-6 mt-2">${folderName}</h5>
+    </div>`;
+        
+    };
     
+    return htmlDirectories;
+}
+
+function generateFilesHTML(response) {
+    
+    let htmlDirectories = '';
+    let files       = response['files'];
+    for (let indexDirectory = 0; indexDirectory < files.length; indexDirectory++) {
+        htmlDirectories +=             `<div class="col text-center">
+        <img src="${getFileIcon(files[indexDirectory].split('.').at(-1))}" alt="Logo" width="55" height="55"
+            style="margin-top: 10px;">
+        <h5 class="text-dracula fs-6 mt-2">${files[indexDirectory]}</h5>
+    </div>`;
+        
+    };
+    return htmlDirectories;
+}
+
+function  refreshFiles(path_api='/api') {
+   
+    if (path === ''){
+        path = path_api;
+    };
+
     $.ajax({
-        url:'/api/',
+        url: path_api,
         method:'GET',
-        datatype: 'json',
+        dataType: 'json',
         success: function (response) {
 
-            let files_body = document.getElementById('files_body');
-            let directories = response['directories'];
-            let files       = response['files'];
-            let html_directories = '';
-            for (let index_directory = 0; index_directory < directories.length; index_directory++) {
-                html_directories +=             `<div class="col text-center">
-                <img src="/static/img/folder.png" alt="Logo" width="55" height="55"
-                    style="margin-top: 10px;">
-                <h5 class="text-dracula fs-6 mt-2">${directories[index_directory]}</h5>
-            </div>`;
-                
-            };
-            for (let index_directory = 0; index_directory < files.length; index_directory++) {
-                html_directories +=             `<div class="col text-center">
-                <img src="${getFileIcon(files[index_directory].split('.').at(-1))}" alt="Logo" width="55" height="55"
-                    style="margin-top: 10px;">
-                <h5 class="text-dracula fs-6 mt-2">${files[index_directory]}</h5>
-            </div>`;
-                
-            };
+            let filesBody = document.getElementById('files_body');
+            let htmlDirectories = '';
+            let pathDirectory = response['path'].includes('//') === true ? response['path'].replace('//', '/') : response['path'];
+            let directories = generateDirectoriesHTML(response,pathDirectory);
+            let files       = generateFilesHTML(response);
             
-
-            files_body.innerHTML = html_directories;
+            htmlDirectories += directories + files;
+            filesBody.innerHTML = htmlDirectories;
         }
     });
+
+    if (path_api){
+        path = path_api;
+    }
 };
 
 $(document).ready(function() {
+
 
     const socketio = io();
     refreshFiles();   
 
     socketio.on('new_files', (data) => {
-        refreshFiles();    
+        refreshFiles(path);
+        console.log("Valor de la path: " + path);
     });
     
 });
